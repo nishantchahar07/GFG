8f95dabbfdb9a5881a04552039c6d19c093da7cf
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-09-28 20:56:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.java
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.java
@@ -1,41 +1,32 @@
 class Solution {
-    public static boolean isvalid(int[] arr , int mid  , int k){
-        int count = 1;
-        int val =  0;
-        int i = 0;
-        while(i < arr.length){
-            if(val+arr[i] > mid ){
-                count++;
-                val = arr[i];
+    public static boolean isvalid(int[] arr , int mid , int k){
+        int ans = 1; 
+        int count  = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(count + arr[i] > mid){
+                ans++; 
+                count = arr[i]; 
             }
-            else {
-                val+=arr[i];
+            else{
+                count += arr[i];
             }
-            i++;
-            if(count > k)return false;
-            
+            if(ans > k) return false;
         }
         return true;
     }
+
     public int minTime(int[] arr, int k) {
-       int lo =  0;
-       int hi  = 0;
-       for(int i : arr){
-           hi+=i;
-           lo = Math.max(lo,i);
-       }
-       int  ans = -1;
-       while(lo <=  hi){
-           
-           int mid =  lo + (hi-lo)/2;
-           if(isvalid(arr, mid , k)){
-               ans =  mid;
-               hi =  mid-1;
-               
+       int st = Arrays.stream(arr).max().getAsInt(); 
+       int end = Arrays.stream(arr).sum(); 
+       int ans = 0;
+
+       while(st <= end){
+           int mid = st + (end - st)/2;
+           if(isvalid(arr, mid, k)){
+               ans = mid;
+               end = mid - 1; 
            }
-           else{
-            lo = mid+1;   
-           }
+           else st = mid + 1;
        }
        return ans;
     }
