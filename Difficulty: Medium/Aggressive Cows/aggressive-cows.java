866361d43e3be6328083858eaf0e122d5879994a
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-09-28 19:38:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,35 +1,31 @@
 class Solution {
-   public static boolean isvalid(int[] arr, int cow, int mid) {
-    int count = 1;
-    int lastPos = arr[0]; 
-
-    for (int i = 1; i < arr.length; i++) {
-        if (arr[i] - lastPos >= mid) {
-            count++;
-            lastPos = arr[i];
-        }
-        if (count >= cow) return true; 
+    public static boolean isvalid(int[] arr , int mid , int k){
+        int prevcount = arr[0];
+        int count = 1;
+       int i = 1;
+       while(i < arr.length){
+           if(arr[i] -  prevcount >= mid){
+               count++;
+               prevcount = arr[i];
+           }
+          
+           i++;
+       }
+        return count >= k;
     }
-
-    return false;
-}
-
     public int aggressiveCows(int[] stalls, int k) {
-        int lo = 0;
-        int hi = Arrays.stream(stalls).max().getAsInt();
-        int ans = 0;
-        Arrays.sort(stalls);
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
-            if(isvalid(stalls , k , mid)){
-             ans = mid;
-            lo = mid+1;
-        }
-        else{
-            hi = mid-1;
-        }
-        
+       int st = 1;
+       int end = Arrays.stream(stalls).max().getAsInt();
+       int ans = 0;
+       Arrays.sort(stalls);
+       while(st <=  end){
+           int  mid =  st + (end-st)/2;
+           if(isvalid(stalls, mid , k)){
+               ans =  mid;
+               st = mid + 1;
+           }
+           else  end = mid - 1;
+       }
+       return ans;
     }
-     return ans; 
-}
 }
