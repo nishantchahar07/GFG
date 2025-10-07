cf4834db1f96b1915a3593a31c7a96ec150901ce
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-10-07 11:00:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int towerOfHanoi(int n, int from, int to, int aux) {
+      if(n == 0)return 0;
+      
+      int count = towerOfHanoi(n-1 , from , aux , to);
+      count++;
+      count+= towerOfHanoi(n-1 , aux , to , from );
+      return count;
+      
+      
+        
+    }
+}
