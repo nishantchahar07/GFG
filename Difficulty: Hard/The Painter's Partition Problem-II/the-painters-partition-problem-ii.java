f2de733d4f01fe1fbdb59fb17653aff183aa3d60
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-10-10 09:26:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.java
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.java
@@ -1,36 +1,40 @@
+import java.util.*;
 class Solution {
-    public static boolean isvalid(int[] arr , int mid , int k){
-        int ans = 1; 
-        int count  = 0;
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] > mid)return false;
-            if(count + arr[i] > mid){
-                ans++; 
-                count =  arr[i]; 
-                   if(ans > k) return false;
-               
+    public boolean isvalid(int[] arr , int mid , int k){
+        int sum = 0;
+        int count = 1;
+        int i = 0;
+        while( i < arr.length){
+            sum+=arr[i];
+            if(sum > mid){
+            count++;
+             sum =  arr[i];
             }
-            else{
-                count += arr[i];
+            i++;
+            if(count > k){
+                return false;
             }
-         
         }
         return true;
+        
     }
-
     public int minTime(int[] arr, int k) {
-       int st = 0; 
-       int end = Integer.MAX_VALUE; 
-       int ans = 0;
-
-       while(st <= end){
-           int mid = st + (end - st)/2;
-           if(isvalid(arr, mid, k)){
-               ans = mid;
-               end = mid - 1; 
-           }
-           else st = mid + 1;
-       }
-       return ans;
+       int st = Arrays.stream(arr).max().getAsInt();
+        int end = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            end+=arr[i];
+            
+        }
+        int ans = 0;
+        
+        while(st <= end){
+            int mid = st+(end -st)/2;
+            if(isvalid(arr , mid , k)){
+                ans = mid;
+                end =  mid-1;
+            }
+            else st =mid+1;
+        }
+        return ans;
     }
 }
