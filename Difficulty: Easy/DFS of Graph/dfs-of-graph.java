6e6e3fdcc29c7e78c23352c76e5d31b5d4234744
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-01 18:39:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,22 +1,23 @@
 class Solution {
-    public void getdfs(ArrayList<ArrayList<Integer>> adj ,boolean []visited  , 
-    ArrayList<Integer> list , int s){
+    public void  getdfs(boolean[] visited  ,  ArrayList<Integer> list ,ArrayList<ArrayList<Integer>> adj ,  int src){
+        visited[src] =  true;
+        list.add(src);
         
-        visited[s] = true;
-        list.add(s);
-        
-        for(int i : adj.get(s)){
-            if(!visited[i]){
-                visited[i] = true;
-                getdfs(adj , visited  , list  ,  i);
+        for(Integer x : adj.get(src)){
+            if(!visited[x]){
+                getdfs(visited , list ,adj ,  x);
             }
         }
+        
+        
+        
     }
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
-          int size  =  adj.size();
-          boolean []visited  = new boolean[size];
-          ArrayList<Integer> list =  new ArrayList<>();
-         getdfs(adj , visited  , list , 0 );
-         return list;
+   
+       boolean[] visited  =  new boolean[adj.size()];
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        getdfs(visited , list ,adj ,  0);
+        return list;
     }
 }
