f638a24915a6a01eb146691d4f249d0966e87e4f
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-07 12:48:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,26 +1,33 @@
 class Solution {
-    public static boolean get(int[] arr ,  int sum , int n , Boolean [][]dp){
-        
-      
-        if(dp[n][sum] != null)return dp[n][sum];
+    public static boolean get(int[] arr , int sum , int n , Boolean[][] dp ){
         if(sum == 0)return true;
-           if(n == 0)return false;
-    
-        if(arr[n-1] <= sum){
-            
-            Boolean add = get(arr , sum-arr[n-1] , n-1 , dp);
-             Boolean not = get(arr , sum , n-1 , dp);
-            return dp[n][sum] = add || not;
+        if(n ==  0 && sum > 0)return false;
+        if(dp[n][sum] != null){
+            return dp[n][sum];
+        }
+        int val =  arr[n-1];
+        if(val > sum) {
+            return dp[n][sum] = get(arr, sum , n-1,dp);
         }
         else{
-           return dp[n][sum] = get(arr , sum , n-1 , dp);
+            boolean add = get(arr, sum-val , n-1,dp);
+            boolean not = get(arr, sum , n-1,dp);
+            return dp[n][sum] = add || not;
         }
+       
     }
 
     static Boolean isSubsetSum(int arr[], int sum) {
-        int n = arr.length;
-        Boolean [][]dp =  new Boolean[n+1][sum+1];
-        return get(arr , sum , n , dp);
+        
+        int n =  arr.length;
+        int ans = 0;
+        for(int i  : arr){
+            ans+=i;
+        }
+        if(ans < sum)return false;
+        Boolean[][] dp =  new Boolean[arr.length+1][sum+1];
+
+      return get(arr , sum , n, dp);
         
     }
 }
