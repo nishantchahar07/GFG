5770c9b5b7bb85eada9e71884df13a73c06728a2
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-14 16:18:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,40 +1,42 @@
 class Solution {
-    public void getbfs(ArrayList<ArrayList<Integer>> adj,
-                       ArrayList<Integer> ans,
-                       HashSet<Integer> set,
-                       int i) {
-        Queue<Integer> q =  new LinkedList<>();
-    
-          q.add(i);
-          set.add(i);
+    public void getbfs(ArrayList<ArrayList<Integer>> adj,boolean[] visited , Queue<Integer> q , int i , ArrayList<Integer> ll){
+        if(visited[i])return;
         
+        q.add(i);
+        visited[i] =  true;
+      
+        while(!q.isEmpty()){
+            int val =  q.poll();
+          
+            ll.add(val);
+           
             
-            while(!q.isEmpty()){
-                int val =  q.poll();
-               
-
-                ans.add(val);
-               
-                
-                for(int j = 0 ; j < adj.get(val).size() ; j++){
-                    int nbrs =  adj.get(val).get(j);
-                    if(!set.contains(nbrs)){
-                        q.add(nbrs);
-                         set.add(nbrs);
-                    } 
+            for(int j = 0 ;  j < adj.get(val).size() ; j++ ){
+                int nbrs =  adj.get(val).get(j);
+                if(!visited[nbrs]){
+                    q.add(nbrs);
+                     visited[nbrs] =  true;
+                    
                 }
             }
-                       }
+        }
+        
+        
+        
+        
+    }
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        int n =  adj.size(); 
         
-        HashSet<Integer> set = new HashSet<>();
-        ArrayList<Integer> ans =  new ArrayList<>();
+        Queue<Integer> q =  new LinkedList<>();
+        boolean[] visited =  new boolean[n];
+        ArrayList<Integer> ll =  new ArrayList<>();
+        for(int i = 0 ; i<  n ; i++){
+            getbfs(adj , visited ,  q , i , ll);
+        }
+        return ll;
         
-//ignore --> ignore --> visited --> selfwork --> nbrs
-            getbfs(adj, ans, set, 0);  
-
-        return ans; 
-      
+        
         
     }
 }
