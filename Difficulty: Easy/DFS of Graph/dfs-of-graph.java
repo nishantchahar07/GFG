9294336cbd856887c566fa150305bf81030e7802
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-15 17:38:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,27 +1,27 @@
 class Solution {
-    public void getdfs(ArrayList<ArrayList<Integer>> adj , ArrayList<Integer> ll ,boolean[] visited, int i ){
-        if(visited[i])return;
-        
-        ll.add(i);
-        visited[i] = true;
-        
-        for(int j = 0 ; j < adj.get(i).size(); j++){
-            int nbrs =  adj.get(i).get(j);
-            if(!visited[nbrs]){
-                getdfs(adj ,  ll , visited , nbrs);
-            }
-        }
-        
-    }
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
-        int n = adj.size();
-        ArrayList<Integer> ll = new ArrayList<Integer>();
-        boolean[] visited =  new boolean[n];
-        for(int i = 0 ; i < n ; i++ ){
-           getdfs(adj , ll ,  visited ,  i); 
-        }
-        
-        return ll;
+           ArrayList<Integer>  ll =  new ArrayList<>();
+           Stack<Integer>  st = new Stack<>();
+           boolean visited[] =  new boolean[adj.size()];
+          
+          st.push(0);
+          while(!st.isEmpty()){
+              int val =  st.pop();
+              
+              if(visited[val])continue;
+              ll.add(val);
+              visited[val] = true;
+              ArrayList<Integer>  BD =  new ArrayList<>();
+              BD = adj.get(val);
+               Collections.reverse(BD);
+              
+              for(int nbrs :BD){
+                  if(!visited[nbrs]){
+                      st.push(nbrs);
+                  }
+              }
+          }
+          return ll;
         
     }
 }
