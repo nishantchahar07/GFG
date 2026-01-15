6e3f8007b789ab233aeb4e9881d932214b5eadbc
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-15 18:08:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,42 +1,23 @@
 class Solution {
-    public void getbfs(ArrayList<ArrayList<Integer>> adj,boolean[] visited , Queue<Integer> q , int i , ArrayList<Integer> ll){
-        if(visited[i])return;
-        
-        q.add(i);
-        visited[i] =  true;
-      
-        while(!q.isEmpty()){
-            int val =  q.poll();
-          
-            ll.add(val);
-           
-            
-            for(int j = 0 ;  j < adj.get(val).size() ; j++ ){
-                int nbrs =  adj.get(val).get(j);
-                if(!visited[nbrs]){
-                    q.add(nbrs);
-                     visited[nbrs] =  true;
-                    
-                }
-            }
-        }
-        
-        
-        
-        
-    }
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
-        int n =  adj.size(); 
-        
-        Queue<Integer> q =  new LinkedList<>();
-        boolean[] visited =  new boolean[n];
-        ArrayList<Integer> ll =  new ArrayList<>();
-        for(int i = 0 ; i<  n ; i++){
-            getbfs(adj , visited ,  q , i , ll);
-        }
-        return ll;
-        
-        
+       Queue<Integer> q =  new LinkedList<>();
+       boolean visited[] = new boolean[adj.size()];
+       ArrayList<Integer> ll =  new ArrayList<Integer>();
+       
+       q.add(0);
+       
+       while(!q.isEmpty()){
+           int val =  q.poll();
+           if(visited[val])continue;
+           ll.add(val);
+           visited[val] = true;
+           
+           for(int nbrs  : adj.get(val)){
+               q.add(nbrs);
+               
+           }
+       }
+       return ll;
         
     }
 }
